d85c1dd07b0cb4e8076a45175161fe255c5147db
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2022-10-30 20:30:17 +0900

    クラス・コンストラクター・getter

---
+++ training/class.ts
@@ -0,0 +1,19 @@
+class Fraction {
+  constructor(private _numerator: number, private _denominator: number) {
+  }
+
+  get numerator() {
+    return this._numerator
+  }
+
+  get denominator() {
+    return this._denominator
+  }
+}
+
+const f1 = new Fraction(1,2)
+console.log(f1.numerator)
+console.log(f1.denominator)
+
+// f1.numerator = 3
+console.log(f1.numerator)
