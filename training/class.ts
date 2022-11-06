97a7726f55a08f1fc87af2fe100592724fb78859
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2022-11-06 22:50:44 +0900

    メソッドの使い方学習

--- training/class.ts
+++ training/class.ts
@@ -1,5 +1,17 @@
 class Fraction {
-  constructor(private _numerator: number, private _denominator: number) {
+  constructor(private _numerator: number, private _denominator: number) {}
+
+  add(other: Fraction): Fraction {
+    const resultNumerator =
+      this._numerator * other._denominator +
+      this._denominator * other._numerator
+    const resultDenominator = this._denominator * other._denominator
+
+    return new Fraction(resultNumerator, resultDenominator)
+  }
+
+  toString(): string {
+    return `${this._numerator}/${this._denominator}`
   }
 
   get numerator() {
@@ -11,9 +23,17 @@ class Fraction {
   }
 }
 
-const f1 = new Fraction(1,2)
+const f1 = new Fraction(1, 2)
 console.log(f1.numerator)
 console.log(f1.denominator)
 
 // f1.numerator = 3
 console.log(f1.numerator)
+
+const f2 = new Fraction(1, 3)
+console.log(f2.toString())
+
+const result1 = f1.add(f2)
+console.log(result1.toString())
+
+// f1.minus(f2)
