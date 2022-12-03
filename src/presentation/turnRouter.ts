531c867cdaf8616f304cb7540c534afcf31c6c29
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2022-12-03 19:46:55 +0900

    リクエストやレスポンスの型を作成(DTO)
    サービスクラスの型を変更した際に、気がつかないうちにAPIのレスポンスの形式が変わってしまうことがあるため、データのやりとりする入れ物クラスを作成(Data Transfer Object)

--- src/presentation/turnRouter.ts
+++ src/presentation/turnRouter.ts
@@ -1,35 +1,54 @@
 import express from 'express';
-import { GameGateway } from '../dataaccess/gameGateway';
-import { TurnGateway } from '../dataaccess/turnGateway';
-import { MoveGateway } from '../dataaccess/moveGateway';
-import { SquareGateway } from '../dataaccess/squareGateway';
-import { connectMySQL } from '../dataaccess/connection';
 import { TurnService } from '../application/turnService';
 
 export const turnRouter = express.Router()
 
 const turnService = new TurnService()
 
-const gameGateway = new GameGateway()
-const turnGateway = new TurnGateway()
-const moveGateway = new MoveGateway()
-const squareGateway = new SquareGateway()
+interface TurnGetResponseBody {
+  turnCount: number
+  board: number[][]
+  nextDisc: number | null
+  winnerDisc: number | null
+}
 
-turnRouter.get('/api/games/latest/turns/:turnCount', async (req, res) => {
-  const turnCount = parseInt(req.params.turnCount)
+turnRouter.get(
+  '/api/games/latest/turns/:turnCount',
+  async (req, res: express.Response<TurnGetResponseBody>) => {
+    const turnCount = parseInt(req.params.turnCount)
 
-  const output = await turnService.findLatestGameTurnByTurnCount(turnCount)
+    const output = await turnService.findLatestGameTurnByTurnCount(turnCount)
 
-  res.json(output)
-})
+    const responseBody = {
+      turnCount: output.turnCount,
+      board: output.board,
+      nextDisc: output.nextDisc ?? null,
+      winnerDisc: output.winnerDisc ?? null
+    }
 
-turnRouter.post('/api/games/latest/turns', async (req, res) => {
-  const turnCount = parseInt(req.body.turnCount)
-  const disc = parseInt(req.body.move.disc)
-  const x = parseInt(req.body.move.x)
-  const y = parseInt(req.body.move.y)
+    res.json(responseBody)
+  }
+)
 
-  await turnService.registerTurn(turnCount, disc, x, y)
+interface TurnPostRequestBody {
+  turnCount: number
+  move: {
+    disc: number
+    x: number
+    y: number
+  }
+}
 
-  res.status(201).end()
-})
+turnRouter.post(
+  '/api/games/latest/turns',
+  async (req: express.Request<{}, {}, TurnPostRequestBody>, res) => {
+    const turnCount = req.body.turnCount
+    const disc = req.body.move.disc
+    const x = req.body.move.x
+    const y = req.body.move.y
+
+    await turnService.registerTurn(turnCount, disc, x, y)
+
+    res.status(201).end()
+  }
+)
