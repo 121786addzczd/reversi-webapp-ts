bcc3415b860c798f1ee440355ea7002575fe7dde
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2022-12-11 15:17:21 +0900

    dataaccessの箇所をinfrastructureにリネーム
    一方行に依存が流れる構成になっている(レイヤードアーキテクチャ)ためリネーム

--- src/application/gameService.ts
+++ src/application/gameService.ts
@@ -1,4 +1,4 @@
-import { connectMySQL } from '../dataaccess/connection'
+import { connectMySQL } from '../infrastructure/connection'
 import { GameRepository } from '../domain/game/gameRepository'
 import { firstTurn } from '../domain/turn/turn'
 import { Game } from '../domain/game/game'
